f33224d4f0176d5c3937dc7af73903063f3ee747
omar faruk~ <avirus170@gmail.com>
2023-06-10 12:00:59 +0600

    solved problem 3

---
+++ problem3.js
@@ -0,0 +1,16 @@
+/*
+John’s teacher gave him two variables.
+ Each variable contains a string.
+ John’s teacher asked him to combine these two strings and print them in one line. Help John write the program. 
+ Input: The first and the second lines of the input contain the strings. Output: Print the result in one line.
+
+Practice Problem 3
+
+Sample Input: I am going to be an awesome web developer Output: I am going to be an awesome web developer
+*/
+
+const var1 = 'I am going to be';
+const var2 = 'an awesome web developer';
+
+console.log(`${var1} ${var2}`)
+

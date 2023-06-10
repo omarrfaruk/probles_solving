ca1a5d594e5292f07ac8e55c91d8c7c983c78122
omar faruk~ <avirus170@gmail.com>
2023-06-10 12:05:12 +0600

    number 5 problem solved

---
+++ problem4.js
@@ -0,0 +1,16 @@
+/*
+Sarah’s mother is teaching her mathematics.
+ She gave Sarah a number and asked her what the remainder would be if she divided the number by 5. 
+ Help Sarah write the program. Input: The first line of the input contains the number. Output: Print the remainder.
+
+Practice Problem 4
+
+Sample Input: 119
+
+Output: 4
+*/ 
+
+const momGivenNum = 119
+
+const reminder = momGivenNum % 5
+console.log(reminder)
